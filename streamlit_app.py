ab973534d980d0af9c7dbcbd66e27d33a134bae8
Dimeh91 <150003177+Dimeh91@users.noreply.github.com>
2025-04-08 15:00:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,12 +67,12 @@ Utilisez la barre latérale pour naviguer entre les différents modules :
 st.markdown("## 🚀 Accès rapide aux modules")
 col1, col2 = st.columns(2)
 with col1:
-    st.page_link("LCR", label="📊 LCR")
-    st.page_link("EVE", label="📈 EVE")
-    st.page_link("MNI", label="📊 MNI")
+    st.page_link("lcr", label="📊 LCR")
+    st.page_link("eve", label="📈 EVE")
+    st.page_link("mni", label="📊 MNI")
 with col2:
-    st.page_link("NSFR", label="🏦 NSFR")
-    st.page_link("RISQUE_DE_CHANGE", label="💱 Risque de change")
+    st.page_link("nsfr", label="🏦 NSFR")
+    st.page_link("risque_de_change", label="💱 Risque de change")
 
 # ❓ Expander ALM
 with st.expander("❓ Qu’est-ce que l’ALM ?"):
